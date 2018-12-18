b4f6e3763f41902e97711f0c72ba8c851fd20ddf
r1z <rulezzz97@mail.ru>
2018-12-18 19:26:30 +0300

    object contains point instead of intersects

--- src/frogger/model/GameModel.java
+++ src/frogger/model/GameModel.java
@@ -4,311 +4,331 @@ import frogger.utilClasses.GameStaticValues;
 import frogger.utilClasses.Observable;
 import frogger.utilClasses.Observer;
 
+import java.awt.Point;
+import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GameModel extends Thread implements Observable {
 
-    private final List<Observer> observers = new ArrayList<Observer>();
-    private Frog frog;
-    private List<GameObject> gameObjects = new CopyOnWriteArrayList<GameObject>();
-    private int userScore = 0;
+	private final List<Observer> observers = new ArrayList<Observer>();
+	private Frog frog;
+	private List<GameObject> gameObjects = new CopyOnWriteArrayList<GameObject>();
+	private int userScore = 0;
 
-    //TODO
-    private boolean isGameActive = false;
+	// TODO
+	private boolean isGameActive = false;
 
-    public GameModel() {
-        super();
-    }
+	public GameModel() {
+		super();
+	}
 
-    public void setGameActive(boolean isGameActive) {
+	public void setGameActive(boolean isGameActive) {
 
-        this.isGameActive = isGameActive;
-    }
+		this.isGameActive = isGameActive;
+	}
 
-    @Override
-    public void addObserver(Observer observer) {
-        if (this.observers.contains(observer))
-            return;
-        this.observers.add(observer);
-    }
+	@Override
+	public void addObserver(Observer observer) {
+		if (this.observers.contains(observer))
+			return;
+		this.observers.add(observer);
+	}
 
-    @Override
-    public void notifyObservers(ModelChangeData changeData) {
-        for (Observer observer : observers)
-            observer.handleEvent(changeData);
-    }
+	@Override
+	public void notifyObservers(ModelChangeData changeData) {
+		for (Observer observer : observers)
+			observer.handleEvent(changeData);
+	}
 
-    private void initializeVariables() {
-        this.userScore = 0;
-        gameObjects = new ArrayList<GameObject>();
-        isGameActive = false;
-    }
+	private void initializeVariables() {
+		this.userScore = 0;
+		gameObjects = new ArrayList<GameObject>();
+		isGameActive = false;
+	}
 
-    private void gameStart() {
+	private void gameStart() {
 
-        initializeVariables();
+		initializeVariables();
 
-        this.frog = new Frog(GameStaticValues.FROG_START_X, GameStaticValues.FROG_START_Y);
+		this.frog = new Frog(GameStaticValues.FROG_START_X, GameStaticValues.FROG_START_Y);
 
-      //  Thread thread = new Thread(frog);
-    //    thread.start();
-        frog.start();
+		// Thread thread = new Thread(frog);
+		// thread.start();
+		frog.start();
 
-        notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_CREATE, this.frog));
+		notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_CREATE, this.frog));
 
-        gameObjects = new ArrayList<GameObject>();
+		gameObjects = new ArrayList<GameObject>();
 
-        formStartBlocks();
-    }
+		formStartBlocks();
+	}
 
-    private void formStartBlocks() {
-        int rowYPosition;
+	private void formStartBlocks() {
+		int rowYPosition;
 
-        for (int i = 1; i <= GameStaticValues.START_GROUND_ROWS; i++) {
-            rowYPosition = GameStaticValues.GAME_WINDOW_SIZE.height
-                    - i * GameStaticValues.BLOCK_HEIGHT - i * GameStaticValues.ROWS_GAP;
-                //    - i * GameStaticValues.BLOCK_HEIGHT - (i - GameStaticValues.START_GROUND_ROWS) * GameStaticValues.ROWS_GAP;
-            formRowOfBlocks(rowYPosition, ObjectTypes.GROUND);
-        }
+		for (int i = 1; i <= GameStaticValues.START_GROUND_ROWS; i++) {
+			rowYPosition = GameStaticValues.GAME_WINDOW_SIZE.height - i * GameStaticValues.BLOCK_HEIGHT
+					- i * GameStaticValues.ROWS_GAP;
+			// - i * GameStaticValues.BLOCK_HEIGHT - (i -
+			// GameStaticValues.START_GROUND_ROWS) * GameStaticValues.ROWS_GAP;
+			formRowOfBlocks(rowYPosition, ObjectTypes.GROUND);
+		}
 
-        for (int i = GameStaticValues.START_GROUND_ROWS + 1; i <= GameStaticValues.ROWS_COUNT; i++) {
-            rowYPosition = GameStaticValues.GAME_WINDOW_SIZE.height
-                    - i * GameStaticValues.BLOCK_HEIGHT - i * GameStaticValues.ROWS_GAP;
-                //    - i * GameStaticValues.BLOCK_HEIGHT - (i - GameStaticValues.START_GROUND_ROWS) * GameStaticValues.ROWS_GAP;
-            formRowOfBlocks(rowYPosition, null);
-        }
-    }
+		for (int i = GameStaticValues.START_GROUND_ROWS + 1; i <= GameStaticValues.ROWS_COUNT; i++) {
+			rowYPosition = GameStaticValues.GAME_WINDOW_SIZE.height - i * GameStaticValues.BLOCK_HEIGHT
+					- i * GameStaticValues.ROWS_GAP;
+			// - i * GameStaticValues.BLOCK_HEIGHT - (i -
+			// GameStaticValues.START_GROUND_ROWS) * GameStaticValues.ROWS_GAP;
+			formRowOfBlocks(rowYPosition, null);
+		}
+	}
 
-    private void formRowOfBlocks(int yPosition, ObjectTypes allBlocksType) {
+	private void formRowOfBlocks(int yPosition, ObjectTypes allBlocksType) {
 
-        int blocksCount = 0;
-        int blocksSpeed = 0;
+		int blocksCount = 0;
+		int blocksSpeed = 0;
 
-        if (allBlocksType != ObjectTypes.GROUND) {
+		if (allBlocksType != ObjectTypes.GROUND) {
 
-            //TODO скорость блоков  (изменить, если пофикшу проблему со скоростью лягухи)
-            blocksSpeed = (GameStaticValues.RND.nextInt(2 * GameStaticValues.BLOCK_MAX_MOVE_SPEED + 1)
-                    - GameStaticValues.BLOCK_MAX_MOVE_SPEED);
-        }
+			// TODO скорость блоков (изменить, если пофикшу проблему со
+			// скоростью лягухи)
+			blocksSpeed = (GameStaticValues.RND.nextInt(2 * GameStaticValues.BLOCK_MAX_MOVE_SPEED + 1)
+					- GameStaticValues.BLOCK_MAX_MOVE_SPEED);
+		}
 
-        for (int i = 0; i < GameStaticValues.MAX_BLOCKS_COUNT_IN_ROW; i++) {
+		for (int i = 0; i < GameStaticValues.MAX_BLOCKS_COUNT_IN_ROW; i++) {
 
-            if (blocksCount >= GameStaticValues.MAX_BLOCKS_COUNT_IN_ROW) {
-                return;
-            }
+			if (blocksCount >= GameStaticValues.MAX_BLOCKS_COUNT_IN_ROW) {
+				return;
+			}
 
-            ObjectTypes blockType = allBlocksType;
+			ObjectTypes blockType = allBlocksType;
 
+			if (blockType == null) {
 
-            if (blockType == null) {
+				if (GameStaticValues.RND.nextDouble() > 0.6 || (GameStaticValues.MAX_BLOCKS_COUNT_IN_ROW - i
+						+ blocksCount) > GameStaticValues.MAX_BLOCKS_COUNT_IN_ROW) {
 
-                if (GameStaticValues.RND.nextDouble() > 0.6
-                        || (GameStaticValues.MAX_BLOCKS_COUNT_IN_ROW - i + blocksCount)
-                        > GameStaticValues.MAX_BLOCKS_COUNT_IN_ROW) {
+					blocksCount++;
 
-                    blocksCount++;
+					if (GameStaticValues.RND.nextDouble() > 0.7) {
+						blockType = ObjectTypes.LEAF;
+					} else {
+						blockType = ObjectTypes.WOOD;
+					}
 
-                    if (GameStaticValues.RND.nextDouble() > 0.7) {
-                        blockType = ObjectTypes.LEAF;
-                    } else {
-                        blockType = ObjectTypes.WOOD;
-                    }
+				} else {
+					continue;
+				}
 
-                } else {
-                    continue;
-                }
+			} else {
+				blocksCount++;
+			}
 
-            } else {
-                blocksCount++;
-            }
+			GameObject block = new GameObject(1 + i * GameStaticValues.BLOCK_WIDTH, yPosition,
+					GameStaticValues.BLOCK_WIDTH, GameStaticValues.BLOCK_HEIGHT, blockType);
 
-            GameObject block = new GameObject(1 + i * GameStaticValues.BLOCK_WIDTH, yPosition,
-                    GameStaticValues.BLOCK_WIDTH, GameStaticValues.BLOCK_HEIGHT, blockType);
+			block.setDx(blocksSpeed);
 
-            block.setDx(blocksSpeed);
+			gameObjects.add(block);
 
-            gameObjects.add(block);
+			block.start();
 
-            block.start();
+			notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_CREATE, block));
 
-            notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_CREATE, block));
+		}
+	}
 
-        }
-    }
+	// TODO generateNewObjects
+	private void generateNewObjects() {
+		int lastRowY = gameObjects.get(gameObjects.size() - 1).getObjectRectangle().y;
 
-    // TODO generateNewObjects
-    private void generateNewObjects() {
-        int lastRowY = gameObjects.get(gameObjects.size() - 1).getObjectRectangle().y;
+		if (lastRowY > 0) {
+			int newRowY = lastRowY - GameStaticValues.BLOCK_HEIGHT - GameStaticValues.ROWS_GAP;
 
-        if (lastRowY > 0) {
-            int newRowY = lastRowY - GameStaticValues.BLOCK_HEIGHT - GameStaticValues.ROWS_GAP;
+			formRowOfBlocks(newRowY, null);
+		}
 
-            formRowOfBlocks(newRowY, null);
-        }
+	}
 
-    }
+	// TODO deleteDeadObjects
+	private void deleteDeadObjects() {
 
+		for (int i = gameObjects.size() - 1; i >= 0; i--) {
+			GameObject gameObject = gameObjects.get(i);
 
-    //TODO deleteDeadObjects
-    private void deleteDeadObjects() {
+			if (!gameObject.isAlive()) {
 
-        for (int i = gameObjects.size() - 1; i >= 0; i--) {
-            GameObject gameObject = gameObjects.get(i);
+				notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_DESTROY, gameObject, i));
 
-            if (!gameObject.isAlive()) {
+				gameObjects.remove(i);
 
-                notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_DESTROY, gameObject, i));
+			}
+		}
+	}
 
-                gameObjects.remove(i);
+	public void frogJumpLeft() {
+		frog.jump(MoveDirections.LEFT);
+	}
 
-            }
-        }
-    }
+	public void frogJumpRight() {
+		frog.jump(MoveDirections.RIGHT);
+	}
 
+	public void frogJumpUp() {
+		frog.jump(MoveDirections.UP);
+	}
 
-    public void frogJumpLeft() {
-        frog.jump(MoveDirections.LEFT);
-    }
+	public void frogJumpDown() {
+		frog.jump(MoveDirections.DOWN);
+	}
 
-    public void frogJumpRight() {
-        frog.jump(MoveDirections.RIGHT);
-    }
+	public void changeObjectSize(GameObject object, int width, int height) {
+		object.getObjectRectangle().setSize(width, height);
+	}
 
-    public void frogJumpUp() {
-        frog.jump(MoveDirections.UP);
-    }
+	@Override
+	public void start() {
 
-    public void frogJumpDown() {
-        frog.jump(MoveDirections.DOWN);
-    }
+		while (true) {
 
-    public void changeObjectSize(GameObject object, int width, int height) {
-        object.getObjectRectangle().setSize(width, height);
-    }
+			while (!isGameActive) {
+				System.out.println(" ");
+			}
 
-    @Override
-    public void start() {
+			notifyObservers(new ModelChangeData(ModelChangeEvents.GAME_START, null));
 
-        while (true) {
+			gameStart();
 
-            while (!isGameActive) {
-                System.out.println(" ");
-            }
+			double playerHighestPosition = frog.getObjectRectangle().getY();
 
-            notifyObservers(new ModelChangeData(ModelChangeEvents.GAME_START, null));
+			while (frog.isObjectAlive()) {
 
+				boolean isIntersects = false;
 
-            gameStart();
+				// TODO вход generateNewObjects
+				generateNewObjects();
 
-            double playerHighestPosition = frog.getObjectRectangle().getY();
+				notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_MOVE, frog));
 
-            while (frog.isObjectAlive()) {
+				// frog.getObjectRectangle().x, frog.getObjectRectangle().y,
+				// (int) frog.getDx());
 
-                boolean isIntersects = false;
+				// TODO userScore
+				if (frog.isGravityActive()) {
+					playerHighestPosition += GameStaticValues.GRAVITY;
+				}
 
-                //TODO вход generateNewObjects
-                generateNewObjects();
+				// TODO deleteDeadObjects раскомментить
+				deleteDeadObjects();
 
-                notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_MOVE, frog));
+				if (frog.isGravityActive()) {
+					for (GameObject gameObject : gameObjects) {
+						// gameObject.getObjectRectangle().y +=
+						// GameStaticValues.BLOCK_HEIGHT;
+						gameObject.setDy(GameStaticValues.GRAVITY);
 
-                //    frog.getObjectRectangle().x, frog.getObjectRectangle().y, (int) frog.getDx());
+						/*
+						 * notifyObservers(new
+						 * ModelChangeData(ModelChangeEvents.OBJECT_MOVE,
+						 * gameObject, gameObjects.indexOf(gameObject)));
+						 */
+					}
+				} else if (frog.isCanJump()
+						&& frog.getObjectRectangle().y < GameStaticValues.GAME_WINDOW_SIZE.height / 2) {
 
-                //TODO userScore
-                if (frog.isGravityActive()) {
-                    playerHighestPosition += GameStaticValues.GRAVITY;
-                }
+					// frog.getObjectRectangle().y +=
+					// GameStaticValues.FROG_HEIGHT;
+					frog.setGravityActive(true);
 
+					/*
+					 * notifyObservers(new
+					 * ModelChangeData(ModelChangeEvents.OBJECT_MOVE, frog));
+					 */
 
+				}
 
+				for (GameObject gameObject : gameObjects) {
 
-                //TODO deleteDeadObjects раскомментить
-                deleteDeadObjects();
+					notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_MOVE, gameObject,
+							gameObjects.indexOf(gameObject)));
+				}
 
-                if (frog.isGravityActive()) {
-                    for (GameObject gameObject : gameObjects) {
-                        // gameObject.getObjectRectangle().y += GameStaticValues.BLOCK_HEIGHT;
-                        gameObject.setDy(GameStaticValues.GRAVITY);
+				if (frog.isCanJump()) {
 
-                      /*  notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_MOVE, gameObject,
-                                gameObjects.indexOf(gameObject)));*/
-                    }
-                } else if (frog.isCanJump() &&
-                        frog.getObjectRectangle().y < GameStaticValues.GAME_WINDOW_SIZE.height / 2) {
+					Point frogCenterPoint = new Point((int) frog.getObjectRectangle().getCenterX(),
+							(int) frog.getObjectRectangle().getCenterY());
 
-                    // frog.getObjectRectangle().y += GameStaticValues.FROG_HEIGHT;
-                    frog.setGravityActive(true);
+					// Line2D line = new Line2D.Double((int)
+					// frog.getObjectRectangle().getMinX(),
+					// (int) frog.getObjectRectangle().getCenterY(), (int)
+					// frog.getObjectRectangle().getMaxX(),
+					// (int) frog.getObjectRectangle().getCenterY());
 
+					for (GameObject gameObject : gameObjects) {
 
-                    /* notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_MOVE, frog));*/
+						if (gameObject.getObjectRectangle().contains(frogCenterPoint)) {
 
-                }
+							// if
+							// (gameObject.getObjectRectangle().intersectsLine(line))
+							// {
+							// if
+							// (gameObject.getObjectRectangle().intersects(frog.getObjectRectangle()))
+							// {
 
-                for (GameObject gameObject : gameObjects) {
+							isIntersects = true;
 
-                    notifyObservers(new ModelChangeData(ModelChangeEvents.OBJECT_MOVE, gameObject,
-                            gameObjects.indexOf(gameObject)));
-                }
+							// TODO скорость лягухи на блоке
+							frog.setDx(gameObject.getDx());
+							// frog.stopMovement();
+							// frog.setDx(frog.getDx() + gameObject.getDx());
 
-                if (frog.isCanJump()) {
+							// TODO userScore
 
-                    for (GameObject gameObject : gameObjects) {
+							if (frog.getObjectRectangle().getY() < playerHighestPosition) {
+								// userScore += Math.abs(playerHighestPosition -
+								// frog.getObjectRectangle().getY());
 
-                        if (gameObject.getObjectRectangle().intersects(frog.getObjectRectangle())) {
+								playerHighestPosition = frog.getObjectRectangle().getY();
 
-                            isIntersects = true;
+								switch (gameObject.getObjectType()) {
+								case LEAF:
+									this.userScore += 50;
+									break;
+								case WOOD:
+									this.userScore += 1;
+									break;
+								default:
+									break;
+								}
 
-                            //TODO скорость лягухи на блоке
-                            frog.setDx(gameObject.getDx());
-                            // frog.stopMovement();
-                            // frog.setDx(frog.getDx() + gameObject.getDx());
+								notifyObservers(new ModelChangeData(ModelChangeEvents.SCORE_CHANGE, null, userScore));
+							}
 
+						}
 
-                            //TODO userScore
+					}
 
-                            if (frog.getObjectRectangle().getY() < playerHighestPosition) {
-                              //  userScore += Math.abs(playerHighestPosition - frog.getObjectRectangle().getY());
+				}
 
-                                playerHighestPosition = frog.getObjectRectangle().getY();
+				if (frog.isCanJump() && !isIntersects) {
+					frog.setObjectAlive(false);
+				}
 
-                                switch (gameObject.getObjectType()) {
-                                    case LEAF:
-                                        this.userScore += 50;
-                                        break;
-                                    case WOOD:
-                                        this.userScore += 1;
-                                        break;
-                                    default:
-                                        break;
-                                }
+				try {
+					Thread.sleep(GameStaticValues.THREAD_SLEEP_TIME);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 
-                                notifyObservers(new ModelChangeData(ModelChangeEvents.SCORE_CHANGE, null, userScore));
-                            }
+			}
 
-                        }
+			this.isGameActive = false;
+			notifyObservers(new ModelChangeData(ModelChangeEvents.GAME_OVER, null));
 
-                    }
-
-                }
-
-
-                if (frog.isCanJump() && !isIntersects) {
-                    frog.setObjectAlive(false);
-                }
-
-                try {
-                    Thread.sleep(GameStaticValues.THREAD_SLEEP_TIME);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            this.isGameActive = false;
-            notifyObservers(new ModelChangeData(ModelChangeEvents.GAME_OVER, null));
-
-        }
-    }
+		}
+	}
 
 }
